6ca59808c6fa4e32311a97bf1a2e0c96cc3c770d
lola.medenica <medenica9550@gmail.com>
2024-08-18 15:03:51 +0200

    Implemented an Angular component to display and sort employees by total time worked, with rows highlighted if the total time is less than 100 hours.

---
+++ src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { routes } from './app.routes';
+
+export const appConfig = {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    CommonModule 
+  ]
+};
